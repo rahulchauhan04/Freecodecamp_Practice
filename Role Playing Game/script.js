5fa2aa63009d733318c576f2c8968be24e595bef
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-04-13 22:00:15 +0530

    Update script.js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -159,11 +159,12 @@ function goFight() {
   monsterHealthText.innerText = monsterHealth;
 }
 
+
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
   health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeapon].power
+  monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random()* xp)+ 1;
 }
 
 function dodge() {
